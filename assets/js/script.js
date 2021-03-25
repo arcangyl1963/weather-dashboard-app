7ae5bb786ed405407311c6b844bbdf2970a2ed47
Jim Harris <arcangyl@gmail.com>
2021-03-24 23:19:00 -0400

    Fixed the forecast icon URL in the script.js; updated  styles.css and index.html with responsive classes from bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,131 +11,145 @@ var currHumid = $('#humidity');
 var currWinSpd = $('#windSpd');
 var currUVdex = $('#uvIndex');
 
-
 // validation check to make sure the city name has been stored in local storage already
 function checkCty(city) {
-    for (var i=0; i<savedCity.length; i++) {
-        if(city.toUpperCase()===savedCity[i]) {
-            return -1;
-        }
+  for (var i = 0; i < savedCity.length; i++) {
+    if (city.toUpperCase() === savedCity[i]) {
+      return -1;
     }
-    return 1;
+  }
+  return 1;
 }
 // Store the API key in a variable
 var apiKey = '4a8f1c3d402bd22a309c5913ab421a34';
 
 // Show the current and future weather after city entered
 function showWeather(event) {
-    event.preventDefault();
-    if(srchCity.val().trim()!=='') {
-        enteredCity = srchCity.val().trim();
-        currWeather(enteredCity);
-    }
+  event.preventDefault();
+  if (srchCity.val().trim() !== '') {
+    enteredCity = srchCity.val().trim();
+    currWeather(enteredCity);
+  }
 }
 
 function currWeather(city) {
-    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + enteredCity + '&appid=' + apiKey;
-    $.ajax({url: apiURL, method: 'GET'}).then(function(response) {
+  var apiURL =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    enteredCity +
+    '&appid=' +
+    apiKey;
+  $.ajax({ url: apiURL, method: 'GET' }).then(function (response) {
+    console.log(response);
 
-            console.log(response);
-            
-            var weathIcon = response.weather[0].icon;
-            var weathIconURL = 'https://openweathermap.org/img/wn/' + weathIcon + '@2x.png';
+    var weathIcon = response.weather[0].icon;
+    var weathIconURL =
+      'https://openweathermap.org/img/wn/' + weathIcon + '@2x.png';
 
-            var date = new Date( response.dt*1000).toLocaleDateString();
+    var date = new Date(response.dt * 1000).toLocaleDateString();
 
-            $(currCity).html(response.name +'(' + date + ')' + '<img src=' + weathIconURL + '>');
+    $(currCity).html(
+      response.name + ' (' + date + ')' + '<img src=' + weathIconURL + '>'
+    );
 
-            var tempFrht = (response.main.temp - 273.15) * 1.8 + 32;
-            $(currTemp).html((tempFrht).toFixed(2) + '&#8457');
+    var tempFrht = (response.main.temp - 273.15) * 1.8 + 32;
+    $(currTemp).html(tempFrht.toFixed(2) + '&#8457');
 
-            $(currHumid).html(response.main.humidity + '%');
+    $(currHumid).html(response.main.humidity + '%');
 
-            var windSpd = response.wind.speed;
-            var windSpdMPH = (windSpd * 2.237).toFixed(1);
-            $(currWinSpd).html(windSpdMPH + 'MPH');
+    var windSpd = response.wind.speed;
+    var windSpdMPH = (windSpd * 2.237).toFixed(1);
+    $(currWinSpd).html(windSpdMPH + 'MPH');
 
-            uvIndex(response.coord.lon, response.coord.lat);
-            forecast(response.id);
-            if (response.cod==200) {
-                savedCity = JSON.parse(localStorage.getItem('currCity'));
-                console.log(savedCity);
-                if (savedCity==null) {
-                    savedCity = [];
-                    savedCity.push(enteredCity.toUpperCase());
-                    localStorage.setItem('currCity', JSON.stringify(savedCity));
-                    addToList(enteredCity);
-                } else {
-                    if (find(enteredCity) > 0) {
-                        savedCity.push(enteredCity.toUpperCase());
-                        localStorage.setItem('currCity', JSON.stringify(savedCity));
-                        addToList(enteredCity);
-                    }
-                }
-            }
-
-        });
+    uvIndex(response.coord.lon, response.coord.lat);
+    forecast(response.id);
+    if (response.cod == 200) {
+      savedCity = JSON.parse(localStorage.getItem('currCity'));
+      console.log(savedCity);
+      if (savedCity == null) {
+        savedCity = [];
+        savedCity.push(enteredCity.toUpperCase());
+        localStorage.setItem('currCity', JSON.stringify(savedCity));
+        addToList(enteredCity);
+      } else {
+        if (find(enteredCity) > 0) {
+          savedCity.push(enteredCity.toUpperCase());
+          localStorage.setItem('currCity', JSON.stringify(savedCity));
+          addToList(enteredCity);
+        }
+      }
     }
+  });
+}
 function uvIndex(ln, lt) {
-    var queryuvURL = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + apiKey + '&lat=' + lt + '&lon=' + ln;
-    $.ajax({url: queryuvURL, method: 'GET'}).then(function(response){
-        $(currUVdex).html(response.value);
-    });
+  var queryuvURL =
+    'https://api.openweathermap.org/data/2.5/uvi?appid=' +
+    apiKey +
+    '&lat=' +
+    lt +
+    '&lon=' +
+    ln;
+  $.ajax({ url: queryuvURL, method: 'GET' }).then(function (response) {
+    $(currUVdex).html(response.value);
+  });
 }
 
-function forecast(cityID){
-    var EoD = false;
-    var queryfCastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=' + cityID + '&appid=' + apiKey;
-    $.ajax({url: queryfCastURL, method: 'GET'}).then(function(response){
-        for (i=0; i<5; i++) {
-            var fDate = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
-            var fIconCode = response.list[((i + 1) * 8) - 1].weather[0].icon;
-            var fIconURL = 'https://api.openweathermap.org/img/wn/' + fIconCode + '.png';
-            var tempKel = response.list[((i + 1) * 8) - 1].main.temp;
-            var tempFar = (((tempKel - 273.5) * 1.80) + 32).toFixed(2);
-            var fHumidity = response.list[((i + 1) * 8) - 1].main.humidity;
+function forecast(cityid) {
+  var EoD = false;
+  var queryfCastURL =
+    'https://api.openweathermap.org/data/2.5/forecast?id=' +
+    cityid +
+    '&appid=' +
+    apiKey;
+  $.ajax({ url: queryfCastURL, method: 'GET' }).then(function (response) {
+    for (i = 0; i < 5; i++) {
+      var fDate = new Date ((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+      var fIconCode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+      var fIconURL = 'https://openweathermap.org/img/wn/' + fIconCode + '.png';
+      var tempKel = response.list[((i + 1) * 8) - 1].main.temp;
+      var tempFar = ((tempKel - 273.5) * 1.8 + 32).toFixed(2);
+      var fHumidity = response.list[((i + 1) * 8) - 1].main.humidity;
 
-            $('#fcDate' + i).html(fDate);
-            $('#fcImage' + i).html('<img src=' + fIconURL + ".png");
-            $('#fcTemp' + i).html(tempFar + '&#8457');
-            $('#fcHumid' + i).html(fHumidity + "%");
-        }
-    });
+      $('#fcDate' + i).html(fDate);
+      $('#fcImage' + i).html('<img src=' + fIconURL + '>');
+      $('#fcTemp' + i).html(tempFar + '&#8457');
+      $('#fcHumid' + i).html(fHumidity + '%');
+    }
+  });
 }
 function addToList(cty) {
-    var myListEL = $('<li>' + cty.toUpperCase() + "</li>");
-    $(myListEL).attr('class', 'listItemGrp');
-    $(myListEL).attr('data-value', cty.toUpperCase());
-    $('.listGroup').append(myListEL);
+  var myListEL = $('<li>' + cty.toUpperCase() + '</li>');
+  $(myListEL).attr('class', 'list-group-item');
+  $(myListEL).attr('data-value', cty.toUpperCase());
+  $('.list-group').append(myListEL);
 }
 
 //Show past search history data when the item in the list is clicked
 function showSrchHstyData(event) {
-    var myLiEl = event.target;
-    if (event.target.matches("li")) {
-        enteredCity = myLiEl.textContent.trim();
-        currWeather(enteredCity);
-    }
+  var myLiEl = event.target;
+  if (event.target.matches('li')) {
+    enteredCity = myLiEl.textContent.trim();
+    currWeather(enteredCity);
+  }
 }
 
 function showLstCty() {
-    $('ul').empty();
-    var savedCity = JSON.parse(localStorage.getItem('currCity'));
-    if (savedCity !== null) {
-        savedCity = JSON.parse(localStorage.getItem('currCity'));
-        for (i=0; i < savedCity.length; i++) {
-            addToList(savedCity[i]);
-        }
-        enteredCity = savedCity[i - 1];
-        currWeather(enteredCity);
+  $('ul').empty();
+  var savedCity = JSON.parse(localStorage.getItem('currCity'));
+  if (savedCity !== null) {
+    savedCity = JSON.parse(localStorage.getItem('currCity'));
+    for (i = 0; i < savedCity.length; i++) {
+      addToList(savedCity[i]);
     }
+    enteredCity = savedCity[i - 1];
+    currWeather(enteredCity);
+  }
 }
 
 function clearHstry(event) {
-    event.preventDefault();
-    savedCity = [];
-    localStorage.removeItem('currCity');
-    document.location.reload();
+  event.preventDefault();
+  savedCity = [];
+  localStorage.removeItem('currCity');
+  document.location.reload();
 }
 
 $('#searchBtn').on('click', showWeather);
